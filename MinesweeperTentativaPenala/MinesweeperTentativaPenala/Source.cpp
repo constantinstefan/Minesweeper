5983cca8ebffba025727913f30ff8980916da6d3
constantinstefan <cosminstefan521@gmail.com>
2017-01-10 23:43:02 +0200

    Am facut BFS + bug fix

--- MinesweeperTentativaPenala/MinesweeperTentativaPenala/Source.cpp
+++ MinesweeperTentativaPenala/MinesweeperTentativaPenala/Source.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <ctime>
 #include <iostream>
+#define vecin coada_x[prim] + deplasare_x[i]][coada_y[prim] + deplasare_y[i]
 using namespace sf;
 using namespace std;
 
@@ -12,7 +14,7 @@ int contor;
 int nrSteaguri = 0;
 int coada_x[1000];
 int coada_y[1000];
-int nrMaximBombe = 160;
+int nrMaximBombe = 200;
 
 bool pozitieValida(int a,int b)
 {
@@ -24,48 +26,71 @@ bool pozitieValida(int a,int b)
 
 void BFS(int a, int b)
 {
-	int deplasareLinie[] = { -1,-1,-1,0,1,1,1,0 };
-	int deplasareColoana[] = {-1,0,1,1,1,0,-1,-1 };
+	int deplasare_x[] = { -1,-1,-1,0,1,1,1,0 };
+	int deplasare_y[] = { -1,0,1,1,1,0,-1,-1 };
 	int prim = 1;
 	int ultim = 1;
 	coada_x[prim] = a;
 	coada_y[prim] = b;
 	vizitat[a][b] = 1;
-	cout <<"prim: "<<coada_x[prim]-2 << " " << coada_y[prim]-2<<" "<<vizitat[coada_x[prim]][coada_y[prim]]<<endl;
+	std::cout << "prim: " << coada_x[prim] - 2 << " " << coada_y[prim] - 2 << " " << vizitat[coada_x[prim]][coada_y[prim]] << endl;
 	while (prim <= ultim)
 	{
+
 		vizitat[coada_x[prim]][coada_y[prim]] = 1;
 		for (int i = 0; i < 8; i++)
-			if (pozitieValida(coada_x[prim] + deplasareLinie[i], coada_y[prim] + deplasareColoana[i]))
+	    {
+			if (pozitieValida(coada_x[prim] + deplasare_x[i], coada_y[prim] + deplasare_y[i]))
 			{
-				afisare[coada_x[prim] + deplasareLinie[i]][coada_y[prim] + deplasareColoana[i]] = matrice[coada_x[prim] + deplasareLinie[i]][coada_y[prim] + deplasareColoana[i]];
-				if ( (matrice[coada_x[prim] + deplasareLinie[i]][coada_y[prim] + deplasareColoana[i]] == 0) && (vizitat[coada_x[prim] + deplasareLinie[i]][coada_y[prim] + deplasareColoana[i]]==0));
+				
+				afisare[vecin] = matrice[vecin];
+
+				if (matrice[vecin] != 0)
+					vizitat[vecin] = 1;
+
+				if (matrice[vecin] == 0 && vizitat[vecin]==0)
 				{
-					vizitat[coada_x[prim] + deplasareLinie[i]][coada_y[prim] + deplasareColoana[i]] = 1;
-					ultim++;
-					coada_x[ultim] = coada_x[prim] + deplasareLinie[i];
-					coada_y[ultim] = coada_y[prim] + deplasareColoana[i];
-					cout << "ultim:" << coada_x[ultim]-2 << " " << coada_y[ultim]-2 <<" "<<vizitat[coada_x[ultim]][coada_y[ultim]]<<endl;
-                    				
-				}
+					++ultim;
+					coada_x[ultim] = coada_x[prim] + deplasare_x[i];
+					coada_y[ultim] = coada_y[prim] + deplasare_y[i];
+					//std::cout << "ultim:" << coada_x[ultim] - 2 << " " << coada_y[ultim] - 2 << " " << vizitat[coada_x[ultim]][coada_y[ultim]] << endl;
 			}
 
-		++prim;
+		}
+
 	}
+		++prim;
+  }
 }
+
 	
 	int main()
 {   
+		enum FazaJoc 
+		{
+			Menu,
+		    Options,
+            Game,
+			GameOver,
+			BestTime
+         };
+
+		FazaJoc faza = Game;
+//stii care e faza?:)
+
     Image icon;
 	icon.loadFromFile("icon.png");
 	icon.createMaskFromColor(Color::White);
 
-	srand(time(0));
+	std::srand(time(0));
 
-	sf::RenderWindow joc(sf::VideoMode(1200, 1200), "Cosmin");
+	sf::RenderWindow joc(sf::VideoMode(1200, 1200), "Minesweeper");
 	joc.setIcon(icon.getSize().x,icon.getSize().y,icon.getPixelsPtr());
 
-	
+   Music muzica;
+   muzica.openFromFile("GetLucky.ogg");
+   muzica.play();
+   muzica.setLoop(true);
 
 	for (int i = 3; i <= 32; i++)
 		for (int j = 3; j <= 32; j++)
@@ -104,81 +129,84 @@ void BFS(int a, int b)
 	Sprite s(t);
 
 
-	cout <<"Nr. steaguri:"<< nrSteaguri << endl;
+	std::cout <<"Nr. steaguri:"<< nrSteaguri << endl;
 	while (joc.isOpen())
 	{
-		Vector2i pozitie = Mouse::getPosition(joc);
-		int x = pozitie.x / pixel;
-		int y = pozitie.y / pixel;
-		
-		
-		
-		sf::Event e;
-		while (joc.pollEvent(e))
+		while (faza == Game)
 		{
-			
-			if (e.type == sf::Event::Closed)
-				joc.close();
-			
-			if (e.type == Event::MouseButtonPressed)
+			Vector2i pozitie = Mouse::getPosition(joc);
+			int x = pozitie.x / pixel;
+			int y = pozitie.y / pixel;
+
+
+
+			sf::Event e;
+			while (joc.pollEvent(e))
 			{
-				
-				if (e.key.code == Mouse::Left && afisare[x][y]!=11) 
-				{ 
-                   	afisare[x][y] = matrice[x][y];
-					
-					if (matrice[x][y] == 9) 
-					{ 
-						cout << "BUM!"<< endl;
-						for (int i = 3; i <= 32; i++)
-							for (int j = 3; j <= 32; j++)
-								if(matrice[i][j]!=9||afisare[i][j]!=11)
-								afisare[i][j] = matrice[i][j];
-						//joc.close(); 
-					}
-					cout << x-2 << " " << y-2 << endl;;
-					//if (matrice[x][y] == 0)  BFS(x, y);
-                 
-				}				
-				
-				if (nrSteaguri > 0)
+
+				if (e.type == sf::Event::Closed)
+					joc.close();
+
+				if (e.type == Event::MouseButtonPressed)
 				{
-					if (e.key.code == Mouse::Right)
+
+					if (e.key.code == Mouse::Left && afisare[x][y] != 11)
 					{
-						if (afisare[x][y] == 10)
+						afisare[x][y] = matrice[x][y];
+
+						if (matrice[x][y] == 9)
 						{
-							afisare[x][y] = 11;
-							--nrSteaguri;
-							cout << "Nr. steaguri:" << nrSteaguri << endl;
+							muzica.stop();
+							std::cout << "BUM!" << endl;
+							for (int i = 3; i <= 32; i++)
+								for (int j = 3; j <= 32; j++)
+									if (matrice[i][j] != 9 || afisare[i][j] != 11)
+										afisare[i][j] = matrice[i][j];
+							//joc.close(); 
 						}
-						else if (afisare[x][y] == 11)
-						{
-							afisare[x][y] = 10;
-							++nrSteaguri;
-							cout << "Nr. steaguri:" << nrSteaguri << endl;
-						 }
+						std::cout << x - 2 << " " << y - 2 << endl;;
+						if (matrice[x][y] == 0 && pozitieValida(x, y))  BFS(x, y);
+
 					}
+
+					if (nrSteaguri > 0)
+					{
+						if (e.key.code == Mouse::Right)
+						{
+							if (afisare[x][y] == 10)
+							{
+								afisare[x][y] = 11;
+								--nrSteaguri;
+								std::cout << "Nr. steaguri:" << nrSteaguri << endl;
+							}
+							else if (afisare[x][y] == 11)
+							{
+								afisare[x][y] = 10;
+								++nrSteaguri;
+								std::cout << "Nr. steaguri:" << nrSteaguri << endl;
+							}
+						}
+					}
+					else { std::cout << "Ai ramas fara steaguri!:("; joc.close(); }
+
+
 				}
-				else { cout << "Ai ramas fara steaguri!:("; joc.close(); }
-			
-			
-			}
-		
-		
-        }
 
-		joc.clear(Color(100,0,0,255)); 
-		
-		for (int i = 3; i < 32; i++)
-			for (int j = 3; j <= 32; j++)
-			{   
-				
-				s.setTextureRect(IntRect(pixel*afisare[i][j], 0, pixel, pixel));
-				s.setPosition(i*pixel, j*pixel);
-				joc.draw(s);
+
 			}
-		joc.display();
+
+			joc.clear(Color(100, 0, 0, 255));
+
+			for (int i = 3; i < 32; i++)
+				for (int j = 3; j <= 32; j++)
+				{
+
+					s.setTextureRect(IntRect(pixel*afisare[i][j], 0, pixel, pixel));
+					s.setPosition(i*pixel, j*pixel);
+					joc.draw(s);
+				}
+			joc.display();
+		}
 	}
-
 	return 0;
 }
